dde7d063f912b629ce966aa0aa6befbff705ef88
Hima Bindu Gorre <139939774+himabindu-gorre@users.noreply.github.com>
2025-07-19 08:44:01 +0530

    Update emailRoutes.js

--- backend/Routes/emailRoutes.js
+++ backend/Routes/emailRoutes.js
@@ -1,11 +1,3 @@
-// import express from "express";
-// import { scheduledEmail }  from "../controller/emailController.js";
-
-// const router = express.Router();
-
-// router.post("/schedule", scheduledEmail);
-
-// export default router;
 import express from "express";
 import { scheduleEmail } from "../controller/emailController.js";
 
